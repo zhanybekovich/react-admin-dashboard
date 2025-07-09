9d6cabfb7561bd6465874a513d7d80e6c1f72c61
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-09 18:05:10 +0600

    add total users chart

--- src/data.ts
+++ src/data.ts
@@ -166,3 +166,21 @@ export const topDealsUsers = [
     amount: "1.560",
   },
 ];
+
+export const chartBoxUser = {
+  color: "#8884d8",
+  icon: "/userIcon.svg",
+  title: "Total Users",
+  number: "11.238",
+  dataKey: "users",
+  percentage: 45,
+  chartData: [
+    { name: "Sun", users: 400 },
+    { name: "Mon", users: 600 },
+    { name: "Tue", users: 500 },
+    { name: "Wed", users: 700 },
+    { name: "Thu", users: 400 },
+    { name: "Fri", users: 500 },
+    { name: "Sat", users: 450 },
+  ],
+};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,25 @@
+import ChartBox from "../../components/chart-box/ChartBox";
 import TopBox from "../../components/top-box/TopBox";
 import "./home.scss";
+import { chartBoxUser } from "../../data";
 function Home() {
   return (
     <div className="home">
       <div className="box box-1">
         <TopBox />
       </div>
-      <div className="box box-2"></div>
-      <div className="box box-3"></div>
-      <div className="box box-4"></div>
-      <div className="box box-5"></div>
+      <div className="box box-2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box-3">
+        <ChartBox />
+      </div>
+      <div className="box box-4">
+        <ChartBox />
+      </div>
+      <div className="box box-5">
+        <ChartBox />
+      </div>
       <div className="box box-6"></div>
       <div className="box box-7"></div>
       <div className="box box-8"></div>
