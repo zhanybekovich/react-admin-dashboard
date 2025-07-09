4c66ce0c319c7ab378b2f5c6713276b09323f264
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-09 12:52:02 +0600

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <span>ReactAdmin</span>
+      <span>&copy; Admin Dashboard Built with React</span>
+    </div>
+  );
 }
 
 export default Footer;
