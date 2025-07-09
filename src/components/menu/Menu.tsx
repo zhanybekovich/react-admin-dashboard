01102e3bc417251d71948095e5636256d648f386
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-09 13:14:57 +0600

    add menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import { menu } from "../../data";
 
 function Menu() {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link className="list-item" to={listItem.url} key={listItem.id}>
+              <img src={listItem.icon} alt="" />
+              <span className="list-item-title">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Menu;
