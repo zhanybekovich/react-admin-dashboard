6a7b5e5e9b56dcd8c92f48222eee9ef1401946a5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-09 13:22:49 +0600

    add grid system

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,18 @@
 import "./home.scss";
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box-1"></div>
+      <div className="box box-2"></div>
+      <div className="box box-3"></div>
+      <div className="box box-4"></div>
+      <div className="box box-5"></div>
+      <div className="box box-6"></div>
+      <div className="box box-7"></div>
+      <div className="box box-8"></div>
+      <div className="box box-9"></div>
+    </div>
+  );
 }
 
 export default Home;
